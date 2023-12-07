7abf4a270e19587b200292a85d5902c36c38a1d9
João Victor Carvalho de Oliveira <victor.oliveira6@ftc.edu.br>
2023-12-07 13:07:49 -0300

    AirBNB

---
+++ AIRBNB Veicular/script.js
@@ -0,0 +1,22 @@
+function apagartexto () {
+    const info = document.querySelector('#info');
+    info.value = '';
+
+    const info1 = document.querySelector('#info1');
+    info1.value = '';
+
+    const info2 = document.querySelector('#info2');
+    info2.value = '';
+
+    const info3 = document.querySelector('#info3');
+    info3.value = '';
+
+    const info4 = document.querySelector('#info4');
+    info4.value = '';
+
+    const info5 = document.querySelector('#info5');
+    info5.value = '';
+
+
+};
+
